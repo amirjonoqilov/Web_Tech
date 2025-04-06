4a5a85213aed21a02c633d7689d507076eec8e43
Amirjon Okilov <amirjonoqilov786@gmail.com>
2025-04-06 21:30:43 +0500

    new branche

--- controllers/api/travel/index.js
+++ controllers/api/travel/index.js
@@ -1,66 +1,29 @@
-// import specific service classes
-const ticket_service = require('../../../services/travel');
 const travel_service = require('../../../services/travel');
-
-// mention the service's needed actions (methods)
-const ticket_controller = {
-    getAll(req, res) {
-        res.json(ticket_service.getAll());
-    },
-    create(req, res) {
-        res.status(201).json(
-            ticket_service.create(req, res)
-        );
-    },
-    update(req, res) {
-        const ticket = ticket_service.update(req.params.id, req.body);
-        
-        if (ticket) {
-            res.json(ticket);
-        } else {
-            res.status(404).send('Ticket not found');
-        }
-    },
-    delete(req, res) {
-        const ticket = ticket_service.getById(req.params.id);
-        
-        if (ticket) {
-            ticket_service.delete(req.params.id);
-            res.status(204).send('Ticket deleted successfully');
-        } else {
-            res.status(404).send('Ticket not found');
-        }
-    }
-};
 
 const travel_controller = {
     getAll(req, res) {
         res.json(travel_service.getAll());
     },
     create(req, res) {
-        res.status(201).json(
-            travel_service.create(req, res)
-        );
+        res.status(201).json(travel_service.create(req, res));
     },
     update(req, res) {
-        const travelPost = travel_service.update(req.params.id, req.body);
-
-        if (travelPost) {
-            res.json(travelPost);
+        const updatedPost = travel_service.update(req.params.id, req.body);
+        if (updatedPost) {
+            res.json(updatedPost);
         } else {
             res.status(404).send('Travel post not found');
         }
     },
     delete(req, res) {
         const travelPost = travel_service.getById(req.params.id);
-
         if (travelPost) {
             travel_service.delete(req.params.id);
             res.status(204).send('Travel post deleted successfully');
         } else {
             res.status(404).send('Travel post not found');
         }
-    }
+    },
 };
 
 module.exports = travel_controller;
--- services/travel/index.js
+++ services/travel/index.js
@@ -1,76 +1,55 @@
 const fs = require('fs');
-
-// Access global mock db file
 const travelPosts = require(global.mock_db);
 
-// Write service method implementations
 const travel_service = {
     getAll() {
         return travelPosts;
     },
     getById(id) {
-        return travelPosts.find(post => post.id == id);
+        return travelPosts.find(post => post.id === id);
     },
     create(req, res) {
-        let new_id = genRandId(6);
-
         const { title, location, photos, experiences } = req.body;
-
-        const new_post = {
-            id: new_id,
+        const newPost = {
+            id: generateId(),
             title,
             location,
-            photos: photos || [],
-            experiences: experiences || '',
+            photos: photos ? photos.split(',').map(photo => photo.trim()) : [],
+            experiences,
         };
-
-        travelPosts.push(new_post);
-
-        writeToFile(travelPosts);
-
-        return new_post;
+        travelPosts.push(newPost);
+        saveToDatabase(travelPosts);
+        return newPost;
     },
     update(id, updateData) {
-        const postIndex = travelPosts.findIndex(post => post.id == id);
-
+        const postIndex = travelPosts.findIndex(post => post.id === id);
         if (postIndex === -1) {
             return null;
         }
-
-        travelPosts[postIndex] = { ...travelPosts[postIndex], ...updateData };
-
-        writeToFile(travelPosts);
-
+        travelPosts[postIndex] = {
+            ...travelPosts[postIndex],
+            ...updateData,
+            photos: updateData.photos ? updateData.photos.split(',').map(photo => photo.trim()) : travelPosts[postIndex].photos,
+        };
+        saveToDatabase(travelPosts);
         return travelPosts[postIndex];
     },
     delete(id) {
-        const index = travelPosts.findIndex(post => post.id == id);
-        if (index === -1) {
+        const postIndex = travelPosts.findIndex(post => post.id === id);
+        if (postIndex === -1) {
             return null;
         }
-        travelPosts.splice(index, 1);
-        writeToFile(travelPosts);
+        travelPosts.splice(postIndex, 1);
+        saveToDatabase(travelPosts);
     },
 };
 
-// Create function for overwriting the db file with updated content
-let writeToFile = async (data) => {
-    await fs.writeFileSync(
-        global.mock_db,
-        JSON.stringify(data, null, 4),
-        'utf8'
-    );
-};
+function saveToDatabase(data) {
+    fs.writeFileSync(global.mock_db, JSON.stringify(data, null, 4), 'utf8');
+}
 
-// Generate random id inspired by uuid
-let genRandId = (count) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < count; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-};
+function generateId() {
+    return Math.random().toString(36).substr(2, 6);
+}
 
 module.exports = travel_service;
